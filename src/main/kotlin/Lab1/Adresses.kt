df96a9f57186a3399470c4c1f93aaf182e268170
Goryunov Dima <mindvorg@gmail.com>
2022-03-19 19:45:23 +0300

    is it ready but without tests?

--- src/main/kotlin/Lab1/Adresses.kt
+++ src/main/kotlin/Lab1/Adresses.kt
@@ -1,24 +1,33 @@
 package Lab1.Adresses
 
-//Индекс1, Название города 1, ул. Название улицы, д. Номер дома
+
 data class Addresses(val index: Int, val nameCity: String, val nameStreet: String, val house: Int) {}
 
-fun parseAddresses(adresses: String): List<Addresses>? {
-    if (adresses.isEmpty()) {
+fun parseAddresses(addresses: String): List<Addresses>? {
+    if (addresses.isEmpty()) {
         return null
     }
     val listOfAddreses = mutableListOf<Addresses>()
-    val splitString = adresses.split('\n')
+    val splitString = addresses.split('\n')
 //Splits this char sequence to a list of strings around occurrences of the specified delimiters. We do this to work with current string
     for (str in splitString) {
         val curStr = str.split(',')
+        //Индекс1, Название города 1, ул. Название улицы, д. Номер дома
         val index = curStr[0].substring(curStr[0].indexOf('.') + 1).toInt()//"."
         val city = curStr[1].substring(curStr[1].indexOf(", ") + 2)//", "
-        val street = curStr[2].substring(curStr[2].indexOf(", ") + 5)//", ул. "
+        val street = curStr[2].substring(curStr[2].indexOf(", ") + 6)//", ул. "
         val house = curStr[3].substring(curStr[3].indexOf(", ") + 4).toInt()//", д. "
-        val address = Addresses(index, city, street, house)
-        listOfAddreses.add(address)
+        val curAddress = Addresses(index, city, street, house)
+        listOfAddreses.add(curAddress)
     }
     return listOfAddreses
 }
+
+fun biggestIndex(addresses: List<Addresses>): Addresses? = addresses.maxByOrNull { it.index }
+
+fun smallestIndex(addresses: List<Addresses>): Addresses? = addresses.minByOrNull { it.index }
+
+fun biggestStreet(addresses: List<Addresses>): Addresses? = addresses.maxByOrNull { it.nameStreet.length }
+
+fun smallestStreet(addresses: List<Addresses>): Addresses? = addresses.minByOrNull { it.nameStreet.length }
 
--- src/main/kotlin/Lab1/Main.kt
+++ src/main/kotlin/Lab1/Main.kt
@@ -1,31 +1,10 @@
 package Lab1
 
-
-enum class Grade(val value: Int) {
-    A(5),
-    B(4),
-    C(3),
-    NOT_FINISHED(0)
-}
-
-data class Course(
-    val title: String,
-    val grade: Grade,
-)
-
-data class Student(
-    val firstName: String,
-    val lastName: String,
-    val age: Int,
-    val emails: List<String>,
-    val phones: List<String>,
-    val courses: List<Course>
-)
+import Lab1.Adresses.*
 
 fun main() {
-    println("Hello World!")
-    println("hi!")
-    val adresses = "1.754291, Санкт-Петербург, ул. Космонавтов, д.47\n" +
+    val addresses2=""
+    val addresses = "1.754291, Санкт-Петербург, ул. Космонавтов, д.47\n" +
             "2.563084, Москва, ул. Ленина, д.13\n" +
             "3.687424, Киров, ул. Энгельса, д.64\n" +
             "4.194192, Анапа, ул. Типанова, д.27\n" +
@@ -45,55 +24,23 @@ fun main() {
             "18.795466, Уфа, ул. Типанова, д.37\n" +
             "19.169130, Владивосток, ул. Шевцова, д.29\n" +
             "20.240555, Киров, ул. Кировская, д.53".trimIndent()
-    val adresses2 = """1.754291, Санкт-Петербург, ул. Космонавтов, д.47
-2.563084, Москва, ул. Ленина, д.13
-3.687424, Киров, ул. Энгельса, д.64
-4.194192, Анапа, ул. Типанова, д.27
-5.532212, Ставрополь, ул. Киевская,	д.64
-6.128925, Уфа, ул. Херсонская, д.63
-7.576277, Москва, ул. Громова, д.88
-8.100785, Минск, ул. Дивенская, д.88
-9.304883, Мурманск, ул. Съезжинская, д.86
-10.976672, Птерозаводск, ул. Труда, д.84
-11.773348, Санкт-Петеребург, ул. Глинка, д.19
-12.728544, Белгород, ул. Шкапина, д.51
-13.176478, Калининград, ул. Звездная, д.84
-14.440636, Москва, ул. Ленина, д.73
-15.383197, Москва, ул. Алтайская, д.2
-16.706150, Санкт-Петербург, ул. Громова, д.16
-17.637562, Минск, ул. Космонавтов, д.64
-18.795466, Уфа, ул. Типанова, д.37
-19.169130, Владивосток, ул. Шевцова, д.29
-20.240555, Киров, ул. Кировская, д.53""".trimIndent()
+    val listOfAddresses: List<Addresses>? = parseAddresses(addresses)
+    if(listOfAddresses!=null){
+    if (biggestIndex(listOfAddresses) != null)
+    {
+        println("the biggest index is:${biggestIndex(listOfAddresses)}")}
+    if (smallestIndex(listOfAddresses) != null)
+    {
+        println("the smallest index is:${smallestIndex(listOfAddresses)}")}
 
-
-    println(adresses.length)
-    val students = loadStudents()
-    val ivans = students.filter { it.firstName == "Ivan" }
-    val adultNotIvans = students.filter { it.firstName != "Ivan" && it.age >= 18 }
-
-    val studentWithContacts = students.filter { it.emails.isNotEmpty() || it.phones.isNotEmpty() }
-    val fullNames = students.map { "${it.firstName} ${it.lastName} - ${it.age}" }
-
-    val studentAndTheirAvgGrade = students.map { student ->
-        val avg = student.courses
-            .filter { course -> course.grade != Grade.NOT_FINISHED }
-            .map { course -> course.grade.value }
-            .average()
-        student to avg
-    }
-
-
-    val splitString = adresses.split('\n')
-    for (str in splitString) {
-        val curStr=str.split(',')
-        val index=curStr[3].substring(curStr[3].indexOf(", ")+4)
-        println("index is:$index")
-    }
-
+    if (biggestStreet(listOfAddresses) != null)
+    {
+        println("the biggest street is:${biggestStreet(listOfAddresses)}")}
+    if (smallestStreet(listOfAddresses) != null)
+    {
+        println("the smallest street is:${smallestStreet(listOfAddresses)}")}
 }
-
-
-fun loadStudents(): List<Student> {
-    return emptyList()
+else
+    {
+        println("null list")}
 }
