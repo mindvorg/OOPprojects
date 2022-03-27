70bad2a019b3439fcf43ff30b2a99076e1183175
Goryunov Dima <mindvorg@gmail.com>
2022-03-27 16:46:14 +0300

    make it more beautiful

--- src/test/kotlin/Lab1/AdressesKtTest.kt
+++ src/test/kotlin/Lab1/AdressesKtTest.kt
@@ -15,9 +15,12 @@ internal class AdressesKtTest {
             2.563084, Москва, ул. Ленина, д.13
             3.687424, Киров, ул. Энгельса, д.64
            """.trimIndent()
-        val listofAddresses = parseAddresses(addresses)
+        val listofAddresses: List<Addresses> = listOf(
+            Addresses(754291, "Санкт-Петербург", "Космонавтов", 47),
+            Addresses(563084, "Москва", "Ленина", 13),
+            Addresses(687424, "Киров", "Энгельса", 64)
+        )
         val nullAdresses = ""
-
         val alsoListOfAddresses: List<Addresses> = listOf(
             Addresses(754291, "Санкт-Петербург", "Космонавтов", 47),
             Addresses(563084, "Москва", "Ленина", 13),
@@ -30,48 +33,44 @@ internal class AdressesKtTest {
 
     @Test
     fun biggestIndex() {
-        val addresses = """
-            1.754291, Санкт-Петербург, ул. Космонавтов, д.47
-            2.563084, Москва, ул. Ленина, д.13
-            3.687424, Киров, ул. Энгельса, д.64
-           """.trimIndent()
-        val listofAddresses = parseAddresses(addresses)
+        val listofAddresses: List<Addresses> = listOf(
+            Addresses(754291, "Санкт-Петербург", "Космонавтов", 47),
+            Addresses(563084, "Москва", "Ленина", 13),
+            Addresses(687424, "Киров", "Энгельса", 64)
+        )
         assertEquals(Lab1.biggestIndex(listofAddresses!!), listofAddresses[0])
         assertEquals(Lab1.biggestIndex(emptyList()), null)
     }
 
     @Test
     fun smallestIndex() {
-        val addresses = """
-            1.754291, Санкт-Петербург, ул. Космонавтов, д.47
-            2.563084, Москва, ул. Ленина, д.13
-            3.687424, Киров, ул. Энгельса, д.64
-           """.trimIndent()
-        val listofAddresses = parseAddresses(addresses)
+        val listofAddresses: List<Addresses> = listOf(
+            Addresses(754291, "Санкт-Петербург", "Космонавтов", 47),
+            Addresses(563084, "Москва", "Ленина", 13),
+            Addresses(687424, "Киров", "Энгельса", 64)
+        )
         assertEquals(Lab1.smallestIndex(listofAddresses!!), listofAddresses[1])
         assertEquals(Lab1.smallestIndex(emptyList()), null)
     }
 
     @Test
     fun biggestStreet() {
-        val addresses = """
-            1.754291, Санкт-Петербург, ул. Космонавтов, д.47
-            2.563084, Москва, ул. Ленина, д.13
-            3.687424, Киров, ул. Энгельса, д.64
-           """.trimIndent()
-        val listofAddresses = parseAddresses(addresses)
+        val listofAddresses: List<Addresses> = listOf(
+            Addresses(754291, "Санкт-Петербург", "Космонавтов", 47),
+            Addresses(563084, "Москва", "Ленина", 13),
+            Addresses(687424, "Киров", "Энгельса", 64)
+        )
         assertEquals(Lab1.biggestStreet(listofAddresses!!), listofAddresses[0])
         assertEquals(Lab1.biggestStreet(emptyList()), null)
     }
 
     @Test
     fun smallestStreet() {
-        val addresses = """
-            1.754291, Санкт-Петербург, ул. Космонавтов, д.47
-            2.563084, Москва, ул. Ленина, д.13
-            3.687424, Киров, ул. Энгельса, д.64
-           """.trimIndent()
-        val listofAddresses = parseAddresses(addresses)
+        val listofAddresses: List<Addresses> = listOf(
+            Addresses(754291, "Санкт-Петербург", "Космонавтов", 47),
+            Addresses(563084, "Москва", "Ленина", 13),
+            Addresses(687424, "Киров", "Энгельса", 64)
+        )
         assertEquals(Lab1.smallestStreet(listofAddresses!!), listofAddresses[1])
         assertEquals(Lab1.smallestStreet(emptyList()), null)
     }
